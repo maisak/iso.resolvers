edfc22d23f965aeee02e352c291bb021753fcb71
Anthony <anthony.maisak@hotmail.com>
2020-01-08 16:05:50 +0200

    Currency codes moved from file to code.

--- src/ISO.4217.Tests/ResolverTests.cs
+++ src/ISO.4217.Tests/ResolverTests.cs
@@ -1,4 +1,3 @@
-using System;
 using ISO._4217;
 using NUnit.Framework;
 
@@ -7,20 +6,21 @@ namespace Tests
     [TestFixture]
     public class ResolverTests
     {
-        /// <summary>
-        /// Not actually a test, shows time needed to load xml file
-        /// </summary>
-        [Test]
-        public void XmlLoadPerformance()
-        {
-            Console.WriteLine($"Spent: {CurrencyCodesResolver.Spent}ms");
-        }
-
         [TestCase("978", "EUR")]
         [TestCase("980", "UAH")]
         [TestCase("840", "USD")]
         [TestCase("000", null)]
         public void GetCodeByNumberTest(string number, string expected)
+        {
+            var code = CurrencyCodesResolver.GetCodeByNumber(number);
+            Assert.AreEqual(expected, code);
+        }
+
+        [TestCase(978, "EUR")]
+        [TestCase(980, "UAH")]
+        [TestCase(840, "USD")]
+        [TestCase(0, null)]
+        public void GetCodeByNumberTest(int number, string expected)
         {
             var code = CurrencyCodesResolver.GetCodeByNumber(number);
             Assert.AreEqual(expected, code);
@@ -47,6 +47,16 @@ namespace Tests
         [TestCase("840", "US Dollar")]
         [TestCase("000", null)]
         public void GetCurrencyNameByNumberTest(string number, string expected)
+        {
+            var code = CurrencyCodesResolver.GetCurrencyNameByNumber(number);
+            Assert.AreEqual(expected, code);
+        }
+
+        [TestCase(978, "Euro")]
+        [TestCase(980, "Hryvnia")]
+        [TestCase(840, "US Dollar")]
+        [TestCase(0, null)]
+        public void GetCurrencyNameByNumberTest(int number, string expected)
         {
             var code = CurrencyCodesResolver.GetCurrencyNameByNumber(number);
             Assert.AreEqual(expected, code);
